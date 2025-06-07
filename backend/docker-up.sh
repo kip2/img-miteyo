3ccc98355b0ad8a4333e55d7eab99f471f32ea2d
kip2 <tqa1no4@gmail.com>
2025-06-07 19:15:33 +0900

    Update backend approach

--- backend/docker-up.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build -t img-miteyo-backend .
-
-docker run -p 8080:8080 img-miteyo-backend
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,27 +1,4 @@
-use std::net::{Ipv4Addr, SocketAddr};
-
-use anyhow::Result;
-use axum::Router;
-use axum::http::StatusCode;
-use axum::routing::get;
-use tokio::net::TcpListener;
-
-pub async fn health_check() -> StatusCode {
-    StatusCode::OK
-}
-
-#[tokio::main]
-async fn main() -> Result<()> {
-    let app = Router::new().route("/health", get(health_check));
-    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
-    let listener = TcpListener::bind(addr).await?;
-    println!("Listening on {}", addr);
-    axum::serve(listener, app).await?;
-    Ok(())
-}
-
-#[tokio::test]
-async fn health_check_works() {
-    let status_code = health_check().await;
-    assert_eq!(status_code, StatusCode::OK);
+fn main() {
+    println!("global: {}", std::env::var("GLOBAL").unwrap());
+    println!("local: {}", std::env::var("LOCAL").unwrap());
 }
