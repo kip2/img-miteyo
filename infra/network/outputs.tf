e5a200593f453731521e298bd63502258ca4503b
kip2 <tqa1no4@gmail.com>
2025-06-17 07:22:49 +0900

    Add outputs of infra/network

--- infra/network/outputs.tf
+++ infra/network/outputs.tf
@@ -1,55 +1,55 @@
-output "book_app_vpc_id" {
-  value = aws_vpc.book_app.id
+output "kore_miteyo_vpc_id" {
+  value = aws_vpc.kore_miteyo.id
 }
 
-output "book_app_db_subnet_ids" {
+output "kore_miteyo_db_subnet_ids" {
   value = [
-    aws_subnet.book_app_db_1a.id,
-    aws_subnet.book_app_db_1c.id
+    aws_subnet.kore_miteyo_db_1a.id,
+    aws_subnet.kore_miteyo_db_1c.id
   ]
 }
 
-output "book_app_db_subnet_arns" {
+output "kore_miteyo_db_subnet_arns" {
   value = [
-    aws_subnet.book_app_db_1a.arn,
-    aws_subnet.book_app_db_1c.arn
+    aws_subnet.kore_miteyo_db_1a.arn,
+    aws_subnet.kore_miteyo_db_1c.arn
   ]
 }
 
-output "book_app_private_subnet_arns" {
+output "kore_miteyo_private_subnet_arns" {
   value = [
-    aws_subnet.book_app_private_1a.arn,
-    aws_subnet.book_app_private_1c.arn
+    aws_subnet.kore_miteyo_private_1a.arn,
+    aws_subnet.kore_miteyo_private_1c.arn
   ]
 }
 
-output "book_app_private_subnet_ids" {
+output "kore_miteyo_private_subnet_ids" {
   value = [
-    aws_subnet.book_app_private_1a.id,
-    aws_subnet.book_app_private_1c.id
+    aws_subnet.kore_miteyo_private_1a.id,
+    aws_subnet.kore_miteyo_private_1c.id
   ]
 }
 
-output "book_app_db_subnet_group_name" {
-  value = aws_db_subnet_group.book_app.name
+output "kore_miteyo_db_subnet_group_name" {
+  value = aws_db_subnet_group.kore_miteyo.name
 }
 
-output "book_app_db_security_group_id" {
-  value = aws_security_group.book_app_db.id
+output "kore_miteyo_db_security_group_id" {
+  value = aws_security_group.kore_miteyo_db.id
 }
 
-output "book_app_redis_security_group_id" {
-  value = aws_security_group.book_app_redis.id
+output "kore_miteyo_redis_security_group_id" {
+  value = aws_security_group.kore_miteyo_redis.id
 }
 
-output "book_app_vpc_connector_security_group_id" {
-  value = aws_security_group.book_app_vpc_connector.id
+output "kore_miteyo_vpc_connector_security_group_id" {
+  value = aws_security_group.kore_miteyo_vpc_connector.id
 }
 
-output "book_app_redis_subnet_group_name" {
-  value = aws_elasticache_subnet_group.book_app_redis.name
+output "kore_miteyo_redis_subnet_group_name" {
+  value = aws_elasticache_subnet_group.kore_miteyo_redis.name
 }
 
-output "book_app_codebuild_security_group_id" {
-  value = aws_security_group.book_app_codebuild.id
+output "kore_miteyo_codebuild_security_group_id" {
+  value = aws_security_group.kore_miteyo_codebuild.id
 }
